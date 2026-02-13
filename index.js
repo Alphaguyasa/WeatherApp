4690bb93ed11097a70c9bc16d4f8e9e0619e1781
Alphaguyasa <alphaguyasa@gmail.com>
2026-02-13 22:13:47 +0300

    entered nothing

--- index.js
+++ index.js
@@ -55,5 +55,6 @@ app.post("/get-weather", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log("");
 });
